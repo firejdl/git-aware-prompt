3d8bf64c5076b879ed274c3e53f386585251aa11
Joey Line <firejdl@gmail.com>
2017-08-30 20:32:40 -0700

    Updated to the way I like it

--- prompt.sh
+++ prompt.sh
@@ -1,32 +1,54 @@
-find_git_branch() {
-  # Based on: http://stackoverflow.com/a/13003854/170413
-  local branch
-  if branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null); then
-    if [[ "$branch" == "HEAD" ]]; then
-      branch='detached*'
+__get_git_branch() {
+    # Based on: http://stackoverflow.com/a/13003854/170413
+    local branch
+    local git_branch
+    if branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null); then
+        if [[ "$branch" == "HEAD" ]]; then
+            branch='detached*'
+        fi
+        git_branch=" ($branch)"
+    else
+        git_branch=""
     fi
-    git_branch="($branch)"
-  else
-    git_branch=""
-  fi
+    echo -ne "$git_branch"
 }
 
-find_git_dirty() {
-  local status=$(git status --porcelain 2> /dev/null)
-  if [[ "$status" != "" ]]; then
-    git_dirty='*'
-  else
-    git_dirty=''
-  fi
+__get_git_dirty() {
+    local status=$(git status --porcelain 2> /dev/null)
+    local git_dirty
+    if [[ ! -z "$status" ]]; then
+        git_dirty=true
+    else
+        git_dirty=false
+    fi
+    echo -ne "$git_dirty"
 }
 
-PROMPT_COMMAND="find_git_branch; find_git_dirty; $PROMPT_COMMAND"
+__prompt_command() {
+    PS1="\[$bldgrn\]\u\[$txtrst\]@\[$bldred\]\h\[$txtrst\]:\[$txtblu\]\w"
 
-# Default Git enabled prompt with dirty state
-# export PS1="\u@\h \w \[$txtcyn\]\$git_branch\[$txtred\]\$git_dirty\[$txtrst\]\$ "
+    local git_branch=$(__get_git_branch)
+    local git_dirty=$(__get_git_dirty)
+    local title_text="${PWD}"
 
-# Another variant:
-# export PS1="\[$bldgrn\]\u@\h\[$txtrst\] \w \[$bldylw\]\$git_branch\[$txtcyn\]\$git_dirty\[$txtrst\]\$ "
+    if [[ ! -z "$git_branch" ]]; then
+        title_text+="$git_branch"
+        if [[ "$git_branch" == " (master)" ]]; then
+            PS1+="\[$bldred\]"
+        else
+            PS1+="\[$txtcyn\]"
+        fi
+        PS1+="$git_branch"
+        if [[ "$git_dirty" == true ]]; then
+            PS1+=" \[$txtred\]✗"
+            title_text+=" ✗"
+        else
+            PS1+=" \[$txtgrn\]✓"
+            title_text+=" ✓"
+        fi
+    fi
+    PS1+="\[$txtrst\] \$ "
+    echo -ne "\033];$title_text\007"
+}
 
-# Default Git enabled root prompt (for use with "sudo -s")
-# export SUDO_PS1="\[$bakred\]\u@\h\[$txtrst\] \w\$ "
+export PROMPT_COMMAND=__prompt_command
